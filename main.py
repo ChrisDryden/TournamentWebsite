fb94b9ae060bcde6601ebb2d55202fabd82d63e8
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-03-25 20:31:30 -0400

    added the registration pages
    no storage for passwords yet

--- main.py
+++ main.py
@@ -10,6 +10,26 @@ def home():
         return render_template('login.html')
     else:
         return "Hello Boss!"
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not session.get('logged_in'):
+        return render_template('register.html')
+    else:
+        return "Hello Boss!"
+
+
+@app.route('/complete_registration', methods=['POST'])
+def complete_registration():
+    if not session.get('logged_in'):
+        if request.form['password'] != '' and request.form['username'] != '':
+            if request.form['password'] == request.form['confirm_password']:
+                flash('Registration Successful')
+                return register()
+        flash('Wrong Password!')
+        return register()
+    else:
+        return "Hello Boss!"
  
 @app.route('/login', methods=['POST'])
 def do_admin_login():
