f04fd1b3b2c50d17bb69eed2f10bbbb7c4234cff
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-03-25 23:16:02 -0400

    changed ports

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ def do_admin_login():
  
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
